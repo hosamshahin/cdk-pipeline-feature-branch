91653e8388d63e3e93ddc59d5800b88b36d25e33
Hossameldin Shahin <hosamlshahin@gmail.com>
2024-01-05 10:18:32 -0500

    Fix codeBuild commands

--- src/infra/cicd/pipeline-construct.ts
+++ src/infra/cicd/pipeline-construct.ts
@@ -22,7 +22,7 @@ export class Pipeline extends Construct {
     const accounts = config['accounts']
     const connectionArn = config['connection_arn']
 
-    const input: cdk.pipelines.IFileSetProducer = CodePipelineSource.connection(
+    const input = CodePipelineSource.connection(
       `${props.githubOrg}/${props.githubRepo}`,
       props.githubBranch,
       { connectionArn }
@@ -66,8 +66,14 @@ export class Pipeline extends Construct {
         "npm ci",
         "npm run build",
         "aws s3 cp $CODEBUILD_SRC_DIR/src/client/src/build s3://$BUCKET_NAME/frontend --recursive",
-        `aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"`,
+        "if [ \"$BRANCH_NAME\" == \"main\" ]; then\n    role_arn=\"arn:aws:iam::$PRD_ACCOUNT_ID:role/admin-role-from-cicd-account\"\nelse\n    role_arn=\"arn:aws:iam::$DEV_ACCOUNT_ID:role/admin-role-from-cicd-account\"\nfi\nsession=\"AssumedRoleSession\"\ntarget_account_region=\"$REGION\"\noutput=$(aws sts assume-role \\\n  --role-arn \"$role_arn\" \\\n  --role-session-name \"$session\" \\\n  --duration-seconds 900 \\\n  --region \"$target_account_region\")\n\naccess_key_id=$(echo \"$output\" | jq -r '.Credentials.AccessKeyId')\nsecret_access_key=$(echo \"$output\" | jq -r '.Credentials.SecretAccessKey')\nsession_token=$(echo \"$output\" | jq -r '.Credentials.SessionToken')\n\nexport AWS_ACCESS_KEY_ID=\"$access_key_id\"\nexport AWS_SECRET_ACCESS_KEY=\"$secret_access_key\"\nexport AWS_SESSION_TOKEN=\"$session_token\"\n\naws cloudfront create-invalidation \\\n  --distribution-id \"$DISTRIBUTION_ID\" \\\n  --paths \"/*\"\n"
       ],
+      env: {
+        'BRANCH_NAME': input.sourceAttribute('BranchName'),
+        'DEV_ACCOUNT_ID': accounts['DEV_ACCOUNT_ID'],
+        'PRD_ACCOUNT_ID': accounts['PRD_ACCOUNT_ID'],
+        'REGION': config['region'],
+      },
       rolePolicyStatements: [
         new iam.PolicyStatement({
           resources: ['arn:aws:s3:::appstage-appstack-bucket*/*'],
