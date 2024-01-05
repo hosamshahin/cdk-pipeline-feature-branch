6f88693837435ca3ea630b40bf3be3032edd03a7
Hossameldin Shahin <hosamlshahin@gmail.com>
2024-01-05 15:41:19 -0500

    Add verify_signature

--- src/infra/lambdas/github_webhook_api/github_webhook.py
+++ src/infra/lambdas/github_webhook_api/github_webhook.py
@@ -18,6 +18,23 @@ ssm_client = boto3.client("ssm")
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise Exception(status_code=403, detail="x-hub-signature-256 header is missing!")
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise Exception(status_code=403, detail="Request signatures didn't match!")
+
 def get_github_webhook_secret_from_secretsmanager(github_webhook_secret):
     response = sm_client.get_secret_value(
         SecretId=github_webhook_secret,
@@ -140,7 +157,7 @@ def delete_stack(branch_name, pipeline_template, dev_account):
 
 def handler(event, context):
     logger.info(json.dumps(event))
-    body = json.loads(event.get("body", {}))
+    body = event.get("body", {})
     headers = json.loads(event.get("headers", {}))
     event_type = headers['X-GitHub-Event']
     ref = body.get("ref", "")
