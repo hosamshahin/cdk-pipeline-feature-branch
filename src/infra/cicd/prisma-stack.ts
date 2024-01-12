0f311de847bfd9c2ae2dc063d41c7f96db2d57bc
Hossameldin Shahin <hosamlshahin@gmail.com>
2024-01-11 21:22:20 -0500

    Remove NAT gateways

--- src/infra/cicd/prisma-stack.ts
+++ src/infra/cicd/prisma-stack.ts
@@ -12,7 +12,9 @@ export class PrismaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const currentAcct = cdk.Stack.of(this).account
     const config = this.node.tryGetContext("config")
+    const accounts = config['accounts']
     const vpc = new ec2.Vpc(this, `Vpc`, {
       maxAzs: 2,
       natGateways: 0
@@ -22,33 +24,87 @@ export class PrismaStack extends cdk.Stack {
 
     const database = new rds.DatabaseInstance(this, "PostgresInstance", {
       engine: rds.DatabaseInstanceEngine.POSTGRES,
+      publiclyAccessible: currentAcct == accounts['PRD_ACCOUNT_ID'] ? false : true,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       vpc,
-      vpcSubnets: vpc.selectSubnets({ subnets: vpc.isolatedSubnets.concat(vpc.privateSubnets) })
+      vpcSubnets: {
+        subnetType: currentAcct == accounts['PRD_ACCOUNT_ID'] ? ec2.SubnetType.PRIVATE_ISOLATED : ec2.SubnetType.PUBLIC
+      }
     });
 
-    database.connections.allowDefaultPortFrom(securityGroup);
+    let migrationRunner: any = {}
 
-    const conn: DatabaseConnectionProps = {
-      host: database.instanceEndpoint.hostname,
-      port: cdk.Token.asString(database.instanceEndpoint.port),
-      engine: database.secret!.secretValueFromJson("engine").toString(),
-      username: database.secret!.secretValueFromJson("username").toString(),
-      password: database.secret!.secretValueFromJson("password").toString(),
+    if (currentAcct == accounts['PRD_ACCOUNT_ID']) {
+      database.connections.allowDefaultPortFrom(securityGroup);
+
+      const rdsProxy = database.addProxy('rdsProxy', {
+        secrets: [database.secret!],
+        debugLogging: true,
+        vpc
+      });
+
+      rdsProxy.connections.allowFrom(securityGroup, ec2.Port.tcp(database.instanceEndpoint.port))
+
+      const conn: DatabaseConnectionProps = {
+        host: rdsProxy.endpoint,
+        port: cdk.Token.asString(database.instanceEndpoint.port),
+        engine: database.secret!.secretValueFromJson("engine").toString(),
+        username: database.secret!.secretValueFromJson("username").toString(),
+        password: database.secret!.secretValueFromJson("password").toString(),
+      }
+
+      migrationRunner = new DockerPrismaFunction(this, "DockerMigrationRunner", {
+        code: lambda.DockerImageCode.fromImageAsset(
+          './src/infra/lambda/prisma', {
+          cmd: ["migration-runner.handler"],
+          platform: Platform.LINUX_AMD64,
+        }),
+        memorySize: 256,
+        timeout: cdk.Duration.minutes(10),
+        vpc,
+        securityGroups: [securityGroup],
+        conn
+      });
+
+      // run database migration during CDK deployment
+      const trigger = new Trigger(this, "MigrationTrigger", {
+        handler: migrationRunner,
+      });
+
+      // make sure migration is executed after the database cluster is available.
+      trigger.node.addDependency(rdsProxy);
+    } else {
+      database.connections.allowDefaultPortFrom(securityGroup);
+
+      const conn: DatabaseConnectionProps = {
+        host: database.instanceEndpoint.hostname,
+        port: cdk.Token.asString(database.instanceEndpoint.port),
+        engine: database.secret!.secretValueFromJson("engine").toString(),
+        username: database.secret!.secretValueFromJson("username").toString(),
+        password: database.secret!.secretValueFromJson("password").toString(),
+      }
+
+      migrationRunner = new DockerPrismaFunction(this, "DockerMigrationRunner", {
+        code: lambda.DockerImageCode.fromImageAsset(
+          './src/infra/lambda/prisma', {
+          cmd: ["migration-runner.handler"],
+          platform: Platform.LINUX_AMD64,
+        }),
+        memorySize: 256,
+        timeout: cdk.Duration.minutes(1),
+        vpc,
+        securityGroups: [securityGroup],
+        conn
+      });
+
+      // run database migration during CDK deployment
+      const trigger = new Trigger(this, "MigrationTrigger", {
+        handler: migrationRunner,
+      });
+
+      // make sure migration is executed after the database cluster is available.
+      trigger.node.addDependency(database);
     }
-
-    const migrationRunner = new DockerPrismaFunction(this, "DockerMigrationRunner", {
-      code: lambda.DockerImageCode.fromImageAsset(
-        './src/infra/lambda/prisma', {
-        cmd: ["migration-runner.handler"],
-        platform: Platform.LINUX_AMD64,
-      }),
-      memorySize: 256,
-      timeout: cdk.Duration.minutes(1),
-      vpc,
-      securityGroups: [securityGroup],
-      conn
-    });
 
     new cdk.CfnOutput(this, `MigrationRunnerLambdaArn`, {
       value: migrationRunner.functionArn
@@ -68,14 +124,5 @@ export class PrismaStack extends cdk.Stack {
       parameterName: config['resourceAttr']['migrationRunnerSecurityGroupId'],
       stringValue: securityGroup.securityGroupId
     })
-
-    // run database migration during CDK deployment
-    const trigger = new Trigger(this, "MigrationTrigger", {
-      handler: migrationRunner,
-    });
-
-
-    // make sure migration is executed after the database cluster is available.
-    trigger.node.addDependency(database);
   }
 }
