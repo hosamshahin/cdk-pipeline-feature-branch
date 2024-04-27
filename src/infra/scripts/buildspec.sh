957bc4c22f8ffe6f7934b4d7ab177d7404584cca
Hossameldin Shahin <hosamlshahin@gmail.com>
2024-04-27 13:10:01 -0400

    Pass branch_name to cdk app

--- src/infra/scripts/buildspec.sh
+++ src/infra/scripts/buildspec.sh
@@ -9,4 +9,6 @@ cd $CODEBUILD_SRC_DIR && npm install projen
 
 # Build and deploy
 # cd $CODEBUILD_SRC_DIR/src/client && npm install && npm run build
+cd $CODEBUILD_SRC_DIR/src/infra/lambda/app/auth && npm install --omit=dev
 cd $CODEBUILD_SRC_DIR && npx cdk synth --output=$CODEBUILD_SRC_DIR/cdk.out -c TargetStack=Pipeline -c BranchName=$BRANCH_NAME
+
